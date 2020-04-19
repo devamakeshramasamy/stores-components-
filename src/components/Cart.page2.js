9126e869df25096b9c2262c2c53e8fcc63e7901e
Devamakesh <47493737+devamakeshr@users.noreply.github.com>
2020-04-19 18:36:43 +0530

    last 19/4/20

--- src/components/Cart.page2.js
+++ src/components/Cart.page2.js
@@ -1,14 +1,14 @@
 import React from 'react'
-
-export default function Cartpage2({name,address,phonenumber}){
+const Cartpage2=(props)=>{
 
     return(
         <div>
-            <h1>{name}</h1>
-            <h2> {address}</h2>
-            <h2> {phonenumber}</h2>
+            <h1>{props.name}</h1>
+            <h2> {props.address}</h2>
+            <h2> {props.phonenumber}</h2>
         </div>
       
     )
 
 }
+export default Cartpage2;
